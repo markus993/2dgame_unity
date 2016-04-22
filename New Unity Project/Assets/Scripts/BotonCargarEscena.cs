cb2fb8eea20f9558b0cf0cfaf6cf0c835ddf035e
Marco Mayor <markus993@hotmail.com>
2016-04-22 00:23:00 -0500

    adicion de portada de juego

---
+++ New Unity Project/Assets/Scripts/BotonCargarEscena.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotonCargarEscena : MonoBehaviour {
+
+	public string nombreEscenaParaCargar = "GameScene";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+        //Camera.main.GetComponent<AudioSource>().Stop();
+        //GetComponent<AudioSource>().Play();
+        //Invoke("CargarNivelJuego", GetComponent<AudioSource>().clip.length);
+
+        Application.LoadLevel(nombreEscenaParaCargar);
+    }
+	
+	void CargarNivelJuego(){
+		Application.LoadLevel (nombreEscenaParaCargar);
+
+	}
+}
